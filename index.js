dc65e3997fdbbe0d155c92a38320f2b0143e6f75
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-07-14 07:34:22 +0200

    Moves to latest ZEIT/Now (major)

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const { json, send } = require('micro')
 const sendSms = require('./lib/send-sms')
 const logger = require('./lib/logger')
 
 async function deliverSms (request, response) {
-  const data = await json(request)
+  const data = request.body
   try {
     const result = await sendSms(data)
     logger('info', ['deliverSms', 'sender', data.sender, 'receivers', data.receivers, 'success'])
-    send(response, 200, result)
+    response.json(result)
   } catch (error) {
     logger('error', ['deliverSms', 'sender', data.sender, 'receivers', data.receivers, error])
-    send(response, 500, error)
+    response.status(500)
+    response.send(error)
   }
 }
 
